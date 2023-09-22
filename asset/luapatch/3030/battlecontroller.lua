8db5f8f14832aeafdfcd9f928027b523944d88d0
ZeroRin <ZeroRin@users.noreply.github.com>
2023-09-22 05:12:48 +0000

    JP | 30100 | data 965914a | dabao 20230921_20_43

--- asset/luapatch/3030/battlecontroller.lua
+++ asset/luapatch/3030/battlecontroller.lua
@@ -7,18 +7,31 @@ local FP = CS.TrueSync.FP
 local GetBoneLocalPos = function(self,curFrame,boneName)
 	local characterdata =  self.parentData
 	if characterdata.isVehicleComponent or characterdata.isVehicle then
+		print(characterdata.switchDistance)
 		return self:GetBoneLocalPos(curFrame,boneName) * characterdata.switchDistance
 	else
 		return self:GetBoneLocalPos(curFrame,boneName)
 	end
 end
+local Scout = function(self)
+	local range = self.data.realtimeRange:AsFloat()
+	if range <= 30 then
+		return
+	end
+	self:Scout()
+end
 local CreateFriendlyCharacter = function(self)
-
+	
 	local v = self:CreateFriendlyCharacter()
-	if CS.GF.Battle.BattleDynamicData.isVehicleBattle then
-		util.hotfix_ex(CS.GF.Battle.BattleMemberData,'GetBoneLocalPos',GetBoneLocalPos)
+	--if CS.GF.Battle.BattleDynamicData.isVehicleBattle then
+	--	util.hotfix_ex(CS.GF.Battle.BattleMemberData,'GetBoneLocalPos',GetBoneLocalPos)
+	--else
+	--	xlua.hotfix(CS.GF.Battle.BattleMemberData,'GetBoneLocalPos',nil)
+	--end
+	if CS.GF.Battle.BattleDynamicData.isRemoteBattle then
+		util.hotfix_ex(CS.GF.Battle.BattleEnemyCharacterManager,'Scout',Scout)
 	else
-		xlua.hotfix(CS.GF.Battle.BattleMemberData,'GetBoneLocalPos',nil)
+		xlua.hotfix(CS.GF.Battle.BattleEnemyCharacterManager,'Scout',nil)
 	end
 	return v
 end
