0de327d2c4c268008e7a94d5c7b61428ed566472
ZeroRin <ZeroRin@users.noreply.github.com>
2023-10-20 08:19:26 +0000

    JP | 30100 | data 1d298de | dabao 20231020_13_14

--- asset/luapatch/3030/squadstatecontroller.lua
+++ asset/luapatch/3030/squadstatecontroller.lua
@@ -30,5 +30,14 @@ local InitUIElements = function(self)
 		txtPoint:GetComponent(typeof(CS.ExText)).text = CS.Data.GetLang(31981);
 	end
 end
+
+local VehicleStateController_InitUIElements = function(self)
+	self:InitUIElements();
+	if CS.HotUpdateController.instance.mUsePlatform == CS.HotUpdateController.EUsePlatform.ePlatform_Tw or CS.HotUpdateController.instance.mUsePlatform == CS.HotUpdateController.EUsePlatform.ePlatform_Korea then
+		local txtPoint = self.transform:Find("VehicleDetail/Main/Detail/ComponentInfo/ComponentRollSkill/ComponentRollSkillTitle/Text_ComponentRollSkil");
+		txtPoint:GetComponent(typeof(CS.ExText)).text = CS.Data.GetLang(290115);
+	end
+end
 util.hotfix_ex(CS.SquadStateController,'Start',Start)
 util.hotfix_ex(CS.DormVehicleVisitLogController,'InitUIElements',InitUIElements)
+util.hotfix_ex(CS.VehicleStateController,'InitUIElements',VehicleStateController_InitUIElements)
